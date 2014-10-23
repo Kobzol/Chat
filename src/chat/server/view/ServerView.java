9d7eb5c0ddf629790d46d8c0279551d0144eb7c4
Kobzol <berykubik@gmail.com>
2014-10-23 22:36:32 +0200

    added launcher class and view

---
+++ src/chat/server/view/ServerView.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.server.view;
+
+import chat.server.gui.MainWindow;
+
+/**
+ *
+ * @author Jakub
+ */
+public class ServerView {
+    private final MainWindow window;
+    
+    public ServerView(MainWindow window) {
+        this.window = window;
+    }
+}
