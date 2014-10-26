0ed41e9bfe21f47ddb2979d3193709f9a6876362
Kobzol <berykubik@gmail.com>
2014-10-26 12:38:39 +0100

    added UDP client connector

---
+++ src/chat/klient/net/UdpClientConnector.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.klient.net;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * Client that can sends and receives messages using UDP (multicast).
+ */
+public class UdpClientConnector implements IClientConnector {
+    private final List<ClientEventListener> eventListeners;
+    private final MulticastSocket socket;
+    
+    private Thread inputListener;
+    
+    public UdpClientConnector(String address, int port) throws IOException {
+        this.eventListeners = new ArrayList<>();
+        this.socket = new MulticastSocket(port);
+        socket.joinGroup(InetAddress.getByName(address));
+        
+        this.startListening();
+    }
+            
+    @Override
+    public boolean write(Serializable serializable) {
+        byte[] buffer = serializable.toString().getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        
+        try
+        {
+            this.socket.send(packet);
+            
+            return true;
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+            
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isConnected() {
+        return this.socket.isConnected();
+    }
+
+    @Override
+    public void addClientEventListener(ClientEventListener listener) {
+        this.eventListeners.add(listener);
+    }
+    
+    private void startListening() {
+        this.inputListener = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try
+                {    
+                    while (!inputListener.isInterrupted())
+                    {
+                        byte[] buffer = new byte[256];
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                        socket.receive(packet);
+                        
+                        receiveMessage(new String(packet.getData(), Charset.defaultCharset()));
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    cleanup();
+                }
+            }
+        });
+        
+        this.inputListener.start();
+    }
+    private void receiveMessage(Serializable object) {
+        for (ClientEventListener listener : this.eventListeners)
+        {
+            listener.onMessageReceived(object);
+        }
+    }
+    
+     private void notifyClientDisconnected() {
+        for (ClientEventListener listener : this.eventListeners)
+        {
+            listener.onClientDisconnected();
+        }
+    }
+    
+    /**
+     * Cleans up resources.
+     */
+    private void cleanup() {
+        try
+        {
+            if (this.inputListener.isAlive())
+            {
+                this.inputListener.interrupt();
+            }
+            
+            this.socket.leaveGroup(this.socket.getInetAddress());
+            
+            if (!this.socket.isClosed())
+            {
+                this.socket.close();
+            }
+        }
+        catch (Exception e)
+        {
+            
+        }
+        finally
+        {
+            this.notifyClientDisconnected();
+        }
+    }
+}
